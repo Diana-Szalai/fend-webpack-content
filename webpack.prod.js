2df801957741c544c2a745b70e0937f921ed088d
Diana <diana.szalai@yahoo.com>
2020-07-09 12:43:51 +0300

    Mode prod and mode dev set-up

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'production',
     module: {
         rules: [
             {
